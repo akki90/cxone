64b19dcb481233f98f3e08e1eddeaa7d3bfe8d09
akki90 <akki18990@gmail.com>
2023-09-22 15:11:21 +0530

    Car.py

---
+++ car.py
@@ -0,0 +1,36 @@
+# class Car:
+#     def __init__(self,make,model,year):
+#         self.make=make
+#         self.model=model
+#         self.year=year
+
+#     def descriptive_name(self):
+#         description=self.make + ' ' + self.model + ' ' + str(self.year)
+#         return description
+    
+# cars=Car("Altroz","XM PLUS",2019)
+# print(cars.descriptive_name())
+
+'''
+Setting a Default Value for an Attribute:Odometer Reading
+
+'''
+
+class Car:
+
+    def __init__(self,make,model,year):
+        self.make=make
+        self.model=model
+        self.year=year
+        self.odometer_reading= 0
+
+    def descriptive_name(self):
+        description=self.make + ' ' + self.model + ' ' + str(self.year)
+        return description
+    
+    def odometer_read(self):
+        print(f"The odometer reading for the above car is {str(self.odometer_reading)} miles/hour")
+    
+cars=Car("Altroz","XM PLUS",2019)
+print(cars.descriptive_name())
+cars.odometer_read()
